5bacb2ac8986edfa28f20485b4589386116fce86
dangrtx <dangrtx@gmail.com>
2025-02-12 19:48:27 +0700

    Second commit

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    <h3>User Login</h3>
+
+    <h3>Admin Login</h3>
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
