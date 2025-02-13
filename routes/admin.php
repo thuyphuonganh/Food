61a4197c9a838bc6a6a0ed56607caf0fd4c39b0e
dangrtx <dangrtx@gmail.com>
2025-02-13 09:11:19 +0700

    Reset Password

--- routes/admin.php
+++ routes/admin.php
@@ -12,11 +12,6 @@ use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["middleware" => "guest:admin", "prefix" => "admin", "as" => "admin."],function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
